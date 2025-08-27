dececd6a4c22ffd3a23a6322ab0fa69ead77c0c2
Gonzalo <gonzaloleonelcusmai@gmail.com>
2025-08-27 17:50:32 -0300

    Arquitectura 5 capas

---
+++ DAL/Acceso.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Data;
+using System.Data;
+
+namespace DAL
+{
+    public class Acceso
+    {
+        SqlConnection conexion;
+
+        public void Abrir()
+        {
+            conexion = new SqlConnection(@"INTEGRATED SECURITY=SSPI; DATA SOURCE=DESKTOP-14CDBSR\SQLEXPRESS; INITIAL CATALOG=Parcial2CusmaiGonzalo; TrustServerCertificate=True");
+            conexion.Open();
+        }
+        public void Cerrar()
+        {
+            conexion.Close();
+            conexion = null;
+            GC.Collect();
+        }
+        public SqlCommand CrearComando(string sql, List<SqlParameter> parametros = null)
+        {
+            SqlCommand cmd = new SqlCommand(sql, conexion);
+            cmd.CommandType = CommandType.StoredProcedure;
+            if (parametros != null)
+            {
+                cmd.Parameters.AddRange(parametros.ToArray());
+            }
+            return cmd;
+        }
+        public DataTable Leer(string sql, List<SqlParameter> parametros = null)
+        {
+            SqlDataAdapter adaptador = new SqlDataAdapter();
+            adaptador.SelectCommand = CrearComando(sql, parametros);
+
+            DataTable tabla = new DataTable();
+            adaptador.Fill(tabla);
+            adaptador = null;
+            return tabla;
+        }
+        public int Escribir(string sql, List<SqlParameter> parametros = null)
+        {
+            SqlCommand cmd = CrearComando(sql, parametros);
+            int filas = 0;
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                filas = -1;
+            }
+            return filas;
+        }
+        public SqlParameter CrearParametro(string nombre, string valor)
+        {
+            SqlParameter par = new SqlParameter(nombre, valor);
+            par.DbType = DbType.String;
+            return par;
+        }
+        public SqlParameter CrearParametro(string nombre, int valor)
+        {
+            SqlParameter par = new SqlParameter(nombre, valor);
+            par.DbType = DbType.Int32;
+            return par;
+        }
+    }
+}
