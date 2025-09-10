7a7be88106ac675cca6bdc68aaf99ae295aa2272
Gonzalo <gonzaloleonelcusmai@gmail.com>
2025-09-10 11:36:38 -0300

    BBDD

--- BLL/GestionUsuarios.cs
+++ BLL/GestionUsuarios.cs
@@ -1,4 +1,5 @@
-﻿using Servicios;
+﻿using BE;
+using Servicios;
 
 namespace BLL
 {
@@ -7,7 +8,21 @@ namespace BLL
         DAL.Mapper_usuario mapper = new DAL.Mapper_usuario();
         public void LogearUsuario(BE.USUARIO user)
         {
-            SessionManager.LogIn(user);
+            USUARIO usuarioEncontrado = new USUARIO();
+            usuarioEncontrado = mapper.BuscarUsuario(user);
+            if(usuarioEncontrado == null)
+            {
+                throw new Exception("Usuario o contraseña incorrectos.");
+            }
+            if (CryptoManager.VerificarContraseña(user.Contraseña, usuarioEncontrado.Contraseña, Convert.FromBase64String(usuarioEncontrado.Salt)) == true)
+            {
+                SessionManager.LogIn(user);
+            }
+            else
+            {
+                throw new Exception("Contraseña incorrectos.");
+            }
+            
         }
 
         public void AgregarUsuario(BE.USUARIO nuevoUsuario)
--- GUI/obj/Debug/net8.0-windows/GUI.AssemblyInfo.cs
+++ GUI/obj/Debug/net8.0-windows/GUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b7fa42fad9517fccdba7ad0a420973cf252e311")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23615e8ccfc641249dcb0ae011f0b84ad9a84331")]
 [assembly: System.Reflection.AssemblyProductAttribute("GUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
