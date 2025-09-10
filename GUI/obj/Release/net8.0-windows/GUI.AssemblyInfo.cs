6b7cfeb375e2c01e71a362961c050339ab2f20ff
Gonzalo <gonzaloleonelcusmai@gmail.com>
2025-09-10 17:24:46 -0300

    commit aux 1

--- GUI/obj/Release/net8.0-windows/GUI.AssemblyInfo.cs
+++ GUI/obj/Release/net8.0-windows/GUI.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("GUI")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59b62f033808c957d56278c263ae98e3795609ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("GUI")]
--- Servicios/obj/Debug/net8.0/Servicios.AssemblyInfo.cs
+++ Servicios/obj/Debug/net8.0/Servicios.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Servicios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aef3a0f280c679a7565e0cbf703af3c70be08c6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59b62f033808c957d56278c263ae98e3795609ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Servicios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Servicios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
