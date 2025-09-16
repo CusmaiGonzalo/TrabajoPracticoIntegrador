c9014496329bda32ca560978137833f49fc2da3c
Gonzalo <gonzaloleonelcusmai@gmail.com>
2025-09-16 10:52:29 -0300

    Bitacora v0.2

--- DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8b53af12dc2f737ebcacfeb75d62e5521fa3737")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e602362fa3ecaddd1171fbe5995132c78af55fa1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
